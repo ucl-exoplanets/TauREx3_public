f16b924dc44cdaa61f54ce8ea294d8da2536136d
Primalfido <primalfido@tubgirl.com>
2019-04-16 16:34:24 +0100

    Implementing structure of temperature and atmosphere model

---
+++ taurex/data/atmosphere.py
@@ -0,0 +1,59 @@
+from taurex.log import Logger
+import numpy as np
+
+
+class Atmosphere(Logger):
+    """This class defines the atmosphere
+
+    """
+
+
+
+    def __init__(self,star, planet,nlayers,tp_profile,
+                        gas_profile):
+        super().__init__('Atmosphere')
+
+        self._star = star
+        self._planet = planet
+        self._tp_profile = tp_profile
+        self._gas_profile = gas_profile
+
+    
+
+    def setup_pressure_profile(self,nlayers):
+        """Sets up the pressure profile for the atmosphere model
+        
+        
+        """
+        self._nlayers = nlayers
+
+        # set pressure profile of layer boundaries
+        press_exp = np.linspace(np.log(self.params.atm_min_pres), np.log(self.params.atm_max_pres), self.nlevels)
+        self.pressure_profile_levels =  np.exp(press_exp)[::-1]
+
+        # get mid point pressure between levels (i.e. get layer pressure) computing geometric
+        # average between pressure at n and n+1 level
+        self.pressure_profile = np.power(10, np.log10(self.pressure_profile_levels)[:-1]+
+                                         np.diff(np.log10(self.pressure_profile_levels))/2.)
+
+
+    @property
+    def nLevels(self):
+        return self._nlayers+1
+    
+
+    @property
+    def star(self):
+        return self._star
+    
+    @property
+    def planet(self):
+        return self._planet
+
+    
+    @property
+    def tpProfile(self):
+        return self._tp_profile
+
+
+
---
+++ taurex/data/tpprofiles/isothermal.py
@@ -0,0 +1,35 @@
+from .tpprofile import TPProfile
+import numpy as np
+
+
+class Isothermal(TPProfile):
+    """An isothermal temperature-pressure profile
+
+    Parameters
+    ----------
+    n_layers : :obj:`int`
+        Number of defined layers in the profile
+    
+    temp : :obj:`float`
+        Temperature to set
+
+    """
+
+
+    def __init__(self,n_layers,temp):
+
+        
+        self._tp = np.zeros(shape=(n_layers,))
+
+        self._tp[...] = temp
+
+    
+    def profile(self):
+        """Returns an isothermal temperature-pressure profile
+
+        Returns: :obj:np.array(float)
+            temperature profile
+        """
+        
+        return self._tp
+
