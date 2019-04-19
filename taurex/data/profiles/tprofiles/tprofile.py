f3aab912e7c093ec984ab05395c0376da18034ef
Primalfido <primalfido@tubgirl.com>
2019-04-20 00:29:27 +0100

    Temperature profile can bow be fit

--- taurex/data/profiles/tprofiles/tprofile.py
+++ taurex/data/profiles/tprofiles/tprofile.py
@@ -1,7 +1,7 @@
 from taurex.log import Logger
+from taurex.data.fittable import fitparam,Fittable
 
-
-class TemperatureProfile(Logger):
+class TemperatureProfile(Fittable,Logger):
     """
     Defines temperature profile for an atmosphere
 
@@ -12,8 +12,8 @@ class TemperatureProfile(Logger):
     
 
     def __init__(self,name):
-        super().__init__(name)
-
+        Logger.__init__(self,name)
+        Fittable.__init__(self)
 
 
     def initialize_profile(self,planet,nlayers,pressure_profile):
