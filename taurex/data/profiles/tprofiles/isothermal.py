19621ccf06d8c6a2027c1a9b79984c2cf736a540
Primalfido <primalfido@tubgirl.com>
2019-04-20 00:29:15 +0100

    Iso thermal profile can now be fit

--- taurex/data/profiles/tprofiles/isothermal.py
+++ taurex/data/profiles/tprofiles/isothermal.py
@@ -1,6 +1,6 @@
 from .tprofile import TemperatureProfile
 import numpy as np
-
+from taurex.data.fittable import fitparam
 
 class Isothermal(TemperatureProfile):
     """An isothermal temperature-pressure profile
@@ -14,11 +14,21 @@ class Isothermal(TemperatureProfile):
     """
 
 
-    def __init__(self,iso_temp):
+    def __init__(self,iso_temp=1500):
         super().__init__('Isothermal')
 
         self._iso_temp = iso_temp
     
+
+    @fitparam(param_name='T',param_latex='$T$')
+    def isoTemperature(self):
+        return self._iso_temp
+
+    @isoTemperature.setter
+    def isoTemperature(self,value):
+        self._iso_temp = value
+
+
     def profile(self):
         """Returns an isothermal temperature profile
 
