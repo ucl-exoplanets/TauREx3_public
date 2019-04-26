1264d13abe462659b92c103e9d5fca9b8fcfe70e
Primalfido <primalfido@tubgirl.com>
2019-04-26 15:06:08 +0100

    pressure profile fixed

--- taurex/data/profiles/pressure/pressureprofile.py
+++ taurex/data/profiles/pressure/pressureprofile.py
@@ -21,7 +21,7 @@ class PressureProfile(Fittable,Logger):
 
 class SimplePressureProfile(PressureProfile):
 
-    def __init__(self,nlayers,atm_min_pressure,atm_max_pressure):
+    def __init__(self,nlayers=100,atm_min_pressure=1e-4,atm_max_pressure=1e6):
         super().__init__('pressure_profile',nlayers)
         self.pressure_profile = None
         self._atm_min_pressure = atm_min_pressure
@@ -43,7 +43,22 @@ class SimplePressureProfile(PressureProfile):
                                          np.diff(np.log10(self.pressure_profile_levels))/2.)
 
 
-    @fitparam(param_name='')
+    @fitparam(param_name='min_atm_pressure',param_latex='$P_\mathrm{min}$',default_fit=False,default_bounds=[0.1,1.0])
+    def minAtmospherePressure(self):
+        return self._atm_min_pressure
+    
+    @minAtmospherePressure.setter
+    def minAtmospherePressure(self,value):
+        self._atm_min_pressure = value
+
+    @fitparam(param_name='max_atm_pressure',param_latex='$P_\mathrm{max}$',default_fit=False,default_bounds=[0.1,1.0])
+    def maxAtmospherePressure(self):
+        return self._atm_max_pressure
+    
+    @maxAtmospherePressure.setter
+    def maxAtmospherePressure(self,value):
+        self._atm_max_pressure = value
+
 
     @property
     def profile(self):
