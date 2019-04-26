384a6dabb28dc5d49fb1a961c741aeffb191cc32
Primalfido <primalfido@tubgirl.com>
2019-04-26 14:18:18 +0100

    implemented simple forward model class

--- taurex/model/model.py
+++ taurex/model/model.py
@@ -1,16 +1,43 @@
 from taurex.log import Logger
-
-
+import numpy as np
+import math
 
 class ForwardModel(Logger):
-    """Base forward model class"""
+    """A base class for producing forward models"""
+
+    def __init__(self,name):
+        super().__init__(name)
+
+    
+    
 
 
+    def update_model(self,wn_grid):
+        """Recomputes the model for a specifc wngrid"""
+        raise NotImplementedError
 
-    def __init__(self,atmosphere):
-
-        self._atmosphere = atmosphere
+    
+    def model(self):
+        """Returns the currently computed model"""
+        raise NotImplementedError
 
     
 
-     
+
+class SimpleForwardModel(ForwardModel):
+
+    def __init__(self,name,
+                            planet=None,
+                            star=None,
+                            pressure_profile=None,
+                            temperature_profile=None,
+                            gas_profile=None
+                            ):
+        super().__init__(name)
+
+        self._planet = planet
+        self._star = star
+        self._pressure_profile = pressure_profile
+        self._temperature_profile = temperature_profile
+        self._gas_profile = gas_profile
+
