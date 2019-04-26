602b16fdf88ce18d4f87471e3ef4fe8156165aa8
Primalfido <primalfido@tubgirl.com>
2019-04-26 14:35:25 +0100

    started stellar implementation

---
+++ taurex/data/star.py
@@ -0,0 +1,20 @@
+from taurex.log import Logger
+from taurex.constants import G,RJUP,MJUP
+from .fittable import fitparam,Fittable
+import numpy as np
+
+class Star(Fittable,Logger):
+    """Holds information on the star
+    its default is a blackbody spectra
+
+    Parameters
+    -----------
+
+
+
+    """
+    
+
+    def __init__(self,name,temperature):
+        pass
+        
