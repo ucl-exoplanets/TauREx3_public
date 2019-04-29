a694b04181d48eb485612306db1b7141c1700d34
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2019-04-29 11:53:16 +0100

    fixed up some inits

--- taurex/data/profiles/gas/__init__.py
+++ taurex/data/profiles/gas/__init__.py
@@ -1,3 +1,7 @@
 from .gasprofile import TaurexGasProfile,GasProfile,ComplexGasProfile
 from .constantprofile import ConstantGasProfile
 from .twopointgasprofile import TwoPointGasProfile
+try:
+    from .acegasprofile import ACEGasProfile
+except:
+    print('ACE not detected')
