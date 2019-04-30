90b328950054c50757a1d37faa0e44446915215b
Ahmed Al-Refaie <ahmed@eduroam-int-dhcp-97-197-15.ucl.ac.uk>
2019-04-30 17:05:52 +0100

    successfully collected all info

--- taurex/cia/hitrancia.py
+++ taurex/cia/hitrancia.py
@@ -5,6 +5,46 @@ import numpy as np
 class EndOfHitranCIAException(Exception):
     pass
 
+
+def hashwn(start_wn,end_wn):
+    return str(start_wn)+str(end_wn)
+
+class HitranCiaGrid(object):
+
+    def __init__(self,wn_min,wn_max):
+        self.wn = None
+        self.Tsigma=[]
+
+    def add_temperature(self,T,sigma):
+        self.Tsigma.append((T,sigma))
+
+
+    def find_closest_temperature_index(self,temperature):
+        temp_grid = np.array(self.T)
+        nearest_idx = np.abs(temperature-temp_grid ).argmin()
+        t_idx_min = -1
+        t_idx_max = -1
+        if temp_grid[nearest_idx] > temperature:
+            t_idx_max = nearest_idx
+            t_idx_min = nearest_idx-1
+        else:
+            t_idx_min = nearest_idx
+            t_idx_max = nearest_idx+1
+        return t_idx_min,t_idx_max
+    
+
+    def interp_linear_grid(self,T,t_idx_min,t_idx_max):
+        temp_grid = np.array(self.T)
+        Tmax = temp_grid[t_idx_max]
+        Tmin = temp_grid[t_idx_min]
+        fx0=self.sigma[t_idx_min]
+        fx1 = self.sigma[t_idx_max]
+
+        return fx0 + (fx1-fx0)*(T-Tmin)/(Tmax-Tmin)  
+
+
+
+
 class HitranCIA(CIA):
 
     def __init__(self,filename):
@@ -15,6 +55,7 @@ class HitranCIA(CIA):
         self._wavenumber_grid = None
         self._temperature_grid = None
         self._xsec_grid = None
+        self._wn_dict = {}
         self.load_hitran_file(filename)
 
 
@@ -22,7 +63,7 @@ class HitranCIA(CIA):
 
     def load_hitran_file(self,filename):
         temp_list = []
-        sigma_list = []
+
 
         with open(filename,'r') as f:
             #Read number of points
@@ -32,8 +73,17 @@ class HitranCIA(CIA):
 
                 except EndOfHitranCIAException:
                     break
-                #Append the temperature
-                temp_list.append(T)
+                if not T in temp_list:
+                    temp_list.append(T)
+                
+                wn_hash=hashwn(start_wn,end_wn)
+
+                wn_obj = None
+                if not wn_hash in self._wn_dict:
+                    self._wn_dict[wn_hash] = HitranCiaGrid(start_wn,end_wn)
+                
+                wn_obj=self._wn_dict[wn_hash]
+
                 #Clear the temporary list
                 sigma_temp = []
                 wn_temp = []
@@ -48,12 +98,17 @@ class HitranCIA(CIA):
                     sigma_temp.append(float(_sigma))
                 
                 #Ok we're done lets add the sigma
-                sigma_list.append(np.array(sigma_temp))
+                wn_obj.add_temperature(T,np.array(sigma_temp))
                 #set the wavenumber grid
-                self._wavenumber_grid = np.array(wn_temp)
-            self._temperature_grid = np.array(temp_list)
-            self._xsec_grid = np.array(sigma_list)
-            
+                wn_obj.wn = np.array(wn_temp)
+            #self._temperature_grid = np.array(temp_list)
+            #self._xsec_grid = np.array(sigma_list)
+        
+        self.fill_gaps()
+    def fill_gaps(self):
+        pass
+
+
     def find_closest_temperature_index(self,temperature):
         nearest_idx = np.abs(temperature-self.temperatureGrid).argmin() 
         t_idx_min = -1
