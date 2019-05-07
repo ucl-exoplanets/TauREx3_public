a9c455568688326e5bf08a447a9964fbca39e576
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2019-05-07 17:16:38 +0100

    started main taurex program

--- taurex/taurex.py
+++ taurex/taurex.py
@@ -3,7 +3,31 @@
 
 
 def main():
-    pass
+    import argparse
+    from taurex.parameter import ParameterParser
+    import os
+    import logging
+    import numpy as np
+
+    logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    parser = argparse.ArgumentParser(description='Taurex')
+    parser.add_argument("-i", "--input",dest='input_file',type=str,required=True,help="Input par file to pass")
+    parser.add_argument("-R", "--retrieval",dest='retrieval',type=bool,default=False, help="True=run retrieval, False=runs forward model only")
+    parser.add_argument("-p", "--plot",dest='plot',type=bool,help="Whether to plot after the run")
+
+
+    args=parser.parse_args()
+
+
+    pp = ParameterParser()
+    pp.read(args.input_file)
+
+    model = pp.generate_model()
+
+    print(model)
+    model.build()
+    print(model.model(np.linspace(0,20000,10000)))
+
 
 if __name__=="__main__":
     main()
