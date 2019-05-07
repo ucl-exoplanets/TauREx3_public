3b84cf07a821aae47fdc3434799060dbdff4494b
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2019-05-07 15:29:09 +0100

    refactored to be more generic. Temperature and pressure now setup

--- taurex/parameter/factory.py
+++ taurex/parameter/factory.py
@@ -17,26 +17,7 @@ def get_keywordarg_dict(klass):
     return init_dicts
 
 
-
-def create_gas_profile(config):
-    try:
-        profile_type = config.pop('profile_type').lower()
-    except KeyError:
-        log.error('No profile_type defined for gas profile in input')
-        raise KeyError
-    
-    klass = None
-
-    if profile_type == 'constant':
-        from taurex.data.profiles.gas import ConstantGasProfile
-        klass = ConstantGasProfile
-    elif profile_type== 'twopoint':
-        from taurex.data.profiles.gas import TwoPointGasProfile
-        klass = TwoPointGasProfile
-    elif profile_type == 'ace':
-        from taurex.data.profiles.gas import ACEGasProfile
-        klass = ACEGasProfile
-    
+def create_klass(config,klass):
     kwargs = get_keywordarg_dict(klass)
 
     for key in kwargs.keys():
@@ -44,17 +25,68 @@ def create_gas_profile(config):
             value = config.pop(key)
             kwargs[key] = value
     
-    gas = klass(**kwargs)
+    obj = klass(**kwargs)
 
     for key,value in config.items():
         try:
-            gas[key] = value
+            obj[key] = value
         except KeyError:
-            log.warning('Profile {} does not have parameter {}, skipping'.format(profile_type,key))
+            log.warning('Object {} does not have parameter {}, skipping'.format(klass.__name__,key))
+    return obj
+
+
+def create_profile(config,factory):
+    try:
+        profile_type = config.pop('profile_type').lower()
+    except KeyError:
+        log.error('No profile_type defined input')
+        raise KeyError    
+
+    klass = factory(profile_type)
+
+    obj = create_klass(config,klass)
     
-    return gas
+    return obj
+
+def gas_factory(profile_type):
+    if profile_type == 'constant':
+        from taurex.data.profiles.gas import ConstantGasProfile
+        return ConstantGasProfile
+    elif profile_type== 'twopoint':
+        from taurex.data.profiles.gas import TwoPointGasProfile
+        return TwoPointGasProfile
+    elif profile_type == 'ace':
+        from taurex.data.profiles.gas import ACEGasProfile
+        return ACEGasProfile
+    else:
+        raise NotImplementedError('Gas profile {} not implemented'.format(profile_type))
+
+def temp_factory(profile_type):
+    if profile_type == 'isothermal':
+        from taurex.data.profiles.temperature import Isothermal
+        return Isothermal
+    elif profile_type== 'guillot':
+        from taurex.data.profiles.temperature import Guillot2010
+        return Guillot2010
+    else:
+        raise NotImplementedError('Temperature profile {} not implemented'.format(profile_type))
+
+def pressure_factory(profile_type):
+    if profile_type == 'simple':
+        from taurex.data.profiles.pressure import SimplePressureProfile
+        return SimplePressureProfile
+    else:
+        raise NotImplementedError('Pressure profile {} not implemented'.format(profile_type))
 
 
+def create_gas_profile(config):
+    return create_profile(config,gas_factory)
+
+def create_temperature_profile(config):
+    return create_profile(config,temp_factory)
+
+def create_pressure_profile(config):
+    return create_profile(config,pressure_factory)
 
     
     
