72352e4b9924d5cf3cba28158037d64b083f728d
Ahmed Al-Refaie <alrefaie@desy.de>
2019-05-08 16:11:30 +0100

    fixed bug where wrong planet radius was used

--- taurex/model/transmission.py
+++ taurex/model/transmission.py
@@ -52,7 +52,7 @@ class TransmissionModel(SimpleForwardModel):
         self.debug('Computing path_length: \n z={} \n dz={}'.format(z,dz))
         for layer in range(0,total_layers):
 
-            p = (self._planet.radius+dz[0]/2 + z[layer])**2
+            p = (planet_radius+dz[0]/2 + z[layer])**2
             k = np.zeros(shape=(self.nLayers-layer))
             k[0] = 2.0 * np.sqrt((planet_radius + dz[0]/2. + z[layer] + dz[layer]/2.)**2 - p)
 
@@ -123,7 +123,7 @@ class TransmissionModel(SimpleForwardModel):
         #integral = (self._planet.radius+self.altitudeProfile[:,None])*(1.0-tau)*dz[:,None]
         #integral*=2.0
         #integral = np.sum(integral,axis=0)
-        integral = ne.evaluate('sum((pradius+ap)*(1.0-tau)*_dz*2,axis=0)')
+        integral = ne.evaluate('sum((pradius+ap)*(1.0-tau)*_dz*2.0,axis=0)')
         return ((pradius**2.0) + integral)/(sradius**2),tau
 
 
