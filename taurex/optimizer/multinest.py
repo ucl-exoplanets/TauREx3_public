3705bd2bc42a9d1771c61f7089510890229efc44
Ahmed Al-Refaie <alrefaie@desy.de>
2019-05-13 16:38:53 +0100

    Solutions are read and appeal to the masses included

--- taurex/optimizer/multinest.py
+++ taurex/optimizer/multinest.py
@@ -3,6 +3,8 @@ import pymultinest
 import numpy as np
 import os
 
+from taurex.util.util import read_table,read_error_line,read_error_into_dict,quantile_corner
+
 class MultiNestOptimizer(Optimizer):
 
 
@@ -91,7 +93,7 @@ class MultiNestOptimizer(Optimizer):
                         multimodal=self.multimodes,
                         n_clustering_params=self.nclust_par,
                         max_modes=self.max_modes,
-                        outputfiles_basename=self.dir_multinest,
+                        outputfiles_basename=os.path.join(self.dir_multinest, '1-'),
                         const_efficiency_mode = self.const_eff,
                         importance_nested_sampling = self.imp_sampling,
                         resume = self.resume,
@@ -105,6 +107,7 @@ class MultiNestOptimizer(Optimizer):
         
         self.info('Fit complete.....')
 
+        self._multinest_output = self.store_nest_solutions()
     
     def process_multinest_status(self,status):
         pass
@@ -127,7 +130,7 @@ class MultiNestOptimizer(Optimizer):
         # maximum number of modes
         opt.write_scalar('max_modes',self.max_modes)
         # run in constant efficiency mode
-        opt.write_scalar('constant_effeciency',self.const_eff)
+        opt.write_scalar('constant_efficiency',self.const_eff)
         # set log likelihood tolerance. If change is smaller, multinest will have converged
         opt.write_scalar('evidence_tolerance',self.evidence_tolerance)
         opt.write_scalar('mode_tolerance',self.mode_tolerance)
@@ -141,3 +144,123 @@ class MultiNestOptimizer(Optimizer):
         fit = super().write_fit(output)
 
         return fit
+
+
+    #Laziness brought us to this point
+    #This function is so big and I cannot be arsed to rewrite this in a nicer way, if some angel does it
+    #for me then I will buy them TWO beers.
+    def store_nest_solutions(self):
+        
+
+
+        self.info('Store the multinest results')
+        NEST_out = {}
+        data = np.loadtxt(os.path.join(self.dir_multinest, '1-.txt'))
+
+        NEST_analyzer = pymultinest.Analyzer(n_params=len(self.fitting_parameters),
+                                             outputfiles_basename=os.path.join(self.dir_multinest, '1-'))
+        NEST_stats = NEST_analyzer.get_stats()
+        NEST_out['NEST_stats'] = NEST_stats
+        NEST_out['global_logE'] = (NEST_out['NEST_stats']['global evidence'], NEST_out['NEST_stats']['global evidence error'])
+
+
+        #Bypass if run in multimodes = False. Pymultinest.Analyser does not report means and sigmas in this case
+        if len(NEST_out['NEST_stats']['modes']) == 0:
+            with open(os.path.join(self.dir_multinest, '1-stats.dat')) as file:
+                lines = file.readlines()
+            stats = {'modes': []}
+            read_error_into_dict(lines[0], stats)
+
+            text = ''.join(lines[2:])
+            # without INS:
+            parts = text.split("\n\n")
+            mode = {'index':0}
+
+            modelines = parts[0]
+            t = read_table(modelines,title='Parameter')
+            mode['mean'] = t[:,1].tolist()
+            mode['sigma'] = t[:,2].tolist()
+
+            mode['maximum'] = read_table(parts[1], title='Parameter',d=None)[:,1].tolist()
+            mode['maximum a posterior'] = read_table(parts[2], title='Parameter', d=None)[:, 1].tolist()
+
+            if 'Nested Sampling Global Log-Evidence'.lower() in stats:
+                mode['Local Log-Evidence'.lower()] = stats['Nested Sampling Global Log-Evidence'.lower()]
+                mode['Local Log-Evidence error'.lower()] = stats['Nested Sampling Global Log-Evidence error'.lower()]
+            else:
+                mode['Local Log-Evidence'.lower()] = stats['Nested Importance Sampling Global Log-Evidence'.lower()]
+                mode['Local Log-Evidence error'.lower()] = stats['Nested Importance Sampling Global Log-Evidence error'.lower()]
+
+            mode['Strictly Local Log-Evidence'.lower()] = mode['Local Log-Evidence'.lower()]
+            mode['Strictly Local Log-Evidence error'.lower()] = mode['Local Log-Evidence error'.lower()]
+
+            NEST_out['NEST_stats']['modes'].append(mode)
+
+
+        modes = []
+        modes_weights = []
+        chains = []
+        chains_weights = []
+
+        if self.multimodes:
+
+            # separate modes. get individual samples for each mode
+
+            # get parameter values and sample probability (=weight) for each mode
+            with open(os.path.join(self.dir_multinest, '1-post_separate.dat')) as f:
+                lines = f.readlines()
+                for idx, line in enumerate(lines):
+                    if idx > 2: # skip the first two lines
+                        if lines[idx-1] == '\n' and lines[idx-2] == '\n':
+                            modes.append(chains)
+                            modes_weights.append(chains_weights)
+                            chains = []
+                            chains_weights = []
+                    chain = [float(x) for x in line.split()[2:]]
+                    if len(chain) > 0:
+                        chains.append(chain)
+                        chains_weights.append(float(line.split()[0]))
+                modes.append(chains)
+                modes_weights.append(chains_weights)
+            modes_array = []
+            for mode in modes:
+                mode_array = np.zeros((len(mode), len(mode[0])))
+                for idx, line in enumerate(mode):
+                    mode_array[idx, :] = line
+                modes_array.append(mode_array)
+        else:
+            # not running in multimode. Get chains directly from file 1-.txt
+            modes_array = [data[:,2:]]
+            chains_weights = [data[:,0]]
+            modes_weights.append(chains_weights[0])
+            modes = [0]
+
+        modes_weights = np.asarray(modes_weights)
+
+        for nmode in range(len(modes)):
+            print(nmode)
+
+            mydict = {'type': 'nest',
+                    'local_logE': (NEST_out['NEST_stats']['modes'][0]['local log-evidence'],  NEST_out['NEST_stats']['modes'][0]['local log-evidence error']),
+                    'weights': np.asarray(modes_weights[nmode]),
+                    'tracedata': modes_array[nmode],
+                    'fit_params': {}}
+
+            for idx, param_name in enumerate(self.fit_names):
+
+                trace = modes_array[nmode][:,idx]
+                q_16, q_50, q_84 = quantile_corner(trace, [0.16, 0.5, 0.84],
+                            weights=np.asarray(modes_weights[nmode]))
+                mydict['fit_params'][param_name] = {
+                    'value' : q_50,
+                    'sigma_m' : q_50-q_16,
+                    'sigma_p' : q_84-q_50,
+                    'nest_map': NEST_stats['modes'][nmode]['maximum a posterior'][idx],
+                    'nest_mean': NEST_stats['modes'][nmode]['mean'][idx],
+                    'nest_sigma': NEST_stats['modes'][nmode]['sigma'][idx],
+                    'trace': trace,
+                }
+
+            NEST_out['solutions'].append(mydict)
+        
+        return NEST_out
