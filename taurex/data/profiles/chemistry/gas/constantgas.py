466457ba4dd6e1fb25dbeee17d8067e72b0d4d98
Ahmed Al-Refaie <alrefaie@desy.de>
2019-05-26 19:29:24 +0100

    Implemented first new gas profile and tested

---
+++ taurex/data/profiles/chemistry/gas/constantgas.py
@@ -0,0 +1,41 @@
+from .gas import Gas
+from taurex.util import molecule_texlabel
+class ConstantGas(Gas):
+    """
+
+    Constant gas profile
+
+
+    Parameters
+    -----------
+
+
+    """
+
+
+    def __init__(self,molecule_name='H2O',mix_ratio=1e-5):
+        super().__init__('ConstantGas',molecule_name)
+        self._mix_ratio = mix_ratio
+        self.add_active_gas_param()
+
+    @property
+    def mixProfile(self):
+        return self._mix_ratio
+
+    def add_active_gas_param(self):
+        mol_name = self.molecule
+        param_name = self.molecule
+        param_tex = molecule_texlabel(mol_name)
+        
+        def read_mol(self):
+            return self._mix_ratio
+        def write_mol(self,value):
+            self._mix_ratio = value
+
+        fget = read_mol
+        fset = write_mol
+        
+        bounds = [1.0e-12, 0.1]
+        
+        default_fit = False
+        self.add_fittable_param(param_name,param_tex,fget,fset,'log',default_fit,bounds) 
---
+++ taurex/data/profiles/chemistry/gas/gas.py
@@ -0,0 +1,23 @@
+from taurex.log import Logger
+from taurex.data.fittable import Fittable,fitparam
+
+
+class Gas(Fittable,Logger):
+
+    def __init__(self,name,molecule_name):
+        Logger.__init__(self,name)
+        Fittable.__init__(self)
+        self._molecule_name = molecule_name.upper()
+        self.mix_profile=None
+    
+
+    @property
+    def molecule(self):
+        return self._molecule_name
+
+    @property
+    def mixProfile(self):
+        raise NotImplementedError
+
+    def initialize_profile(self,nlayers,temperature_profile,pressure_profile,altitude_profile):
+        raise NotImplementedError
