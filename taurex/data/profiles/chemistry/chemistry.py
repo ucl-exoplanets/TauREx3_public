f342ae72e9c932491538d6f811b6928b3dcebb1d
Ahmed Al-Refaie <alrefaie@desy.de>
2019-05-26 20:44:56 +0100

    Implemented and tested chemistry model

--- taurex/data/profiles/chemistry/chemistry.py
+++ taurex/data/profiles/chemistry/chemistry.py
@@ -26,12 +26,15 @@ class Chemistry(Fittable,Logger,Writeable):
     
 
     @property
-    def inactiveGases(self):
+    def inActiveGases(self):
         raise NotImplementedError
     
 
     def initialize_chemistry(self,nlayers,temperature_profile,pressure_profile,altitude_profile):
-        raise NotImplementedError
+        
+
+
+        self.compute_mu_profile(nlayers)
 
 
     @property
@@ -39,10 +42,41 @@ class Chemistry(Fittable,Logger,Writeable):
         raise NotImplementedError
 
     @property
-    def inactiveGasMixProfile(self):
+    def inActiveGasMixProfile(self):
         raise NotImplementedError
 
 
     @property
     def muProfile(self):
-        raise NotImplementedError
+        return self.mu_profile
+
+
+    def get_gas_mix_profile(self,gas_name):
+        if gas_name in self.activeGases:
+            idx = self.activeGases.index(gas_name)
+            return self.activeGasMixProfile[idx,:]
+        elif gas_name in self.inActiveGases:
+            idx = self.inActiveGases.index(gas_name)
+            return self.inActiveGasMixProfile[idx,:]  
+        else:
+            raise KeyError  
+
+    def write(self,output):
+
+        gas_entry = output.create_group('Gas')
+        gas_entry.write_string('gas_profile_type',self.__class__.__name__)
+        gas_entry.write_string_array('active_gases',self.activeGases)
+        gas_entry.write_string_array('inactive_gases',self.inActiveGases)
+        gas_entry.write_array('active_gas_mix_profile',self.activeGasMixProfile)
+        gas_entry.write_array('inactive_gas_mix_profile',self.inActiveGasMixProfile)
+        gas_entry.write_array('mu_profile',self.muProfile)
+        return gas_entry
+
+    def compute_mu_profile(self,nlayers):
+        self.mu_profile= np.zeros(shape=(nlayers,))
+        if self.activeGasMixProfile is not None:
+            for idx, gasname in enumerate(self.activeGases):
+                self.mu_profile += self.activeGasMixProfile[idx,:]*get_molecular_weight(gasname)
+        if self.inActiveGasMixProfile is not None:
+            for idx, gasname in enumerate(self.inActiveGases):
+                self.mu_profile += self.inActiveGasMixProfile[idx,:]*get_molecular_weight(gasname)
--- taurex/data/profiles/chemistry/taurexchemistry.py
+++ taurex/data/profiles/chemistry/taurexchemistry.py
@@ -1,10 +1,123 @@
 from .chemistry import Chemistry
-from taurex.external.ace import md_ace
 from taurex.data.fittable import fitparam
 import numpy as np
 import math
+
+from taurex.util import *
 class TaurexChemistry(Chemistry):
 
 
-    def __init__(self):
-        super().__init__('ACE')
+    def __init__(self,mode='absolute',ratio_molecule_index=0,n2_mix_ratio=0,he_h2_ratio=0.1764):
+        super().__init__('ChemistryModel')
+
+        self._mode = mode
+        self._ratio_molecule=ratio_molecule_index
+
+        self._n2_mix_ratio = n2_mix_ratio
+        self._he_h2_mix_ratio = he_h2_ratio
+
+        self._gases = []
+        self.active_mixratio_profile = None
+        self.inactive_mixratio_profile = None
+
+    def addGas(self,gas):
+        self._gases.append(gas)
+
+
+
+
+    @property
+    def ratioMoleculeProfile(self):
+        if self._mode in ('Relative','relative',):
+            return self._gases[self._ratio_molecule].mixRatioProfile
+        else:
+            return 1.0
+
+
+    @property
+    def activeGases(self):
+        return [gas.molecule for gas in self._gases]
+
+
+    @property
+    def inActiveGases(self):
+        return  ['H2', 'HE', 'N2']
+
+
+    def fitting_parameters(self):
+        """Overwrites the fitting paramter with ours"""
+        full_dict = {}
+        for gas in self._gases:
+            full_dict.update(gas.fitting_parameters())
+        
+        full_dict.update(self._param_dict)
+        
+
+        return full_dict
+
+    def initialize_chemistry(self,nlayers,temperature_profile,pressure_profile,altitude_profile):
+        
+        self.active_mixratio_profile = np.zeros(shape=(len(self._gases),nlayers))
+        self.inactive_mixratio_profile = np.zeros((len(self.inActiveGases), nlayers))
+
+        for idx,gas in enumerate(self._gases):
+            gas.initialize_profile(nlayers,temperature_profile,pressure_profile,altitude_profile)
+            self.active_mixratio_profile[idx,:] = gas.mixProfile
+
+
+        
+
+
+        #Since this can either be a scalar one or an array lets do it the old fashion way
+
+
+        self.compute_absolute_gas_profile()
+        
+
+        super().initialize_chemistry(nlayers,temperature_profile,pressure_profile,altitude_profile)
+        
+
+
+    @property
+    def activeGasMixProfile(self):
+        return self.active_mixratio_profile
+
+    @property
+    def inActiveGasMixProfile(self):
+        self.inactive_mixratio_profile
+
+
+    def compute_absolute_gas_profile(self):
+
+        
+        self.inactive_mixratio_profile[2, :] = self._n2_mix_ratio
+        # first get the sum of the mixing ratio of all active gases
+
+
+        active_mixratio_sum = np.sum(self.active_mixratio_profile, axis = 0)
+        
+        active_mixratio_sum += self.inactive_mixratio_profile[2, :]
+        
+
+
+        mixratio_remainder = 1. - active_mixratio_sum
+        self.inactive_mixratio_profile[0, :] = mixratio_remainder/(1. + self._he_h2_mix_ratio) # H2
+        self.inactive_mixratio_profile[1, :] =  self._he_h2_mix_ratio * self.inactive_mixratio_profile[0, :] 
+
+
+
+    @fitparam(param_name='N2',param_latex=molecule_texlabel('N2'),default_mode='log',default_fit=False,default_bounds=[1e-12,1.0])
+    def N2MixRatio(self):
+        return self._n2_mix_ratio
+    
+    @N2MixRatio.setter
+    def N2MixRatio(self,value):
+        self._n2_mix_ratio = value
+
+    @fitparam(param_name='H2_He',param_latex=molecule_texlabel('H$_2$/He'),default_mode='log',default_fit=False,default_bounds=[1e-12,1.0])
+    def H2HeMixRatio(self):
+        return self._he_h2_mix_ratio
+    
+    @H2HeMixRatio.setter
+    def H2HeMixRatio(self,value):
+        self._he_h2_mix_ratio = value
