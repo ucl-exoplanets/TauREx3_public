85b7c4aafc61118e90d2da127bba4f13cb1d9c55
Ahmed Al-Refaie <alrefaie@desy.de>
2019-06-12 16:13:20 +0100

    Dopcstrings for init

---
+++ taurex/data/profiles/__init__.py
@@ -0,0 +1,3 @@
+"""
+These modules contain sub-modules related to defining various profiles in a model
+"""
--- taurex/data/stellar/__init__.py
+++ taurex/data/stellar/__init__.py
@@ -1,2 +1,6 @@
+"""
+Modules relating to defining stellar properties of the model
+"""
+
 from .star import BlackbodyStar
 from .phoenix import PhoenixStar
