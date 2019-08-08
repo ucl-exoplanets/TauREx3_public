3b219958d2a53d10adedb25cbd8cfb6a713bf4bb
Ahmed Al-Refaie <alrefaie@desy.de>
2019-08-09 00:52:17 +0100

    included online variance computer

--- taurex/util/math.py
+++ taurex/util/math.py
@@ -69,5 +69,39 @@ def compute_rayleigh_cross_section(wngrid,n,n_air = 2.6867805e25,king=1.0):
 
     return sigma
 
+class OnlineVariance(object):
+    """USes the M2 algorithm to compute the variance in a streaming fashion"""
 
+    def __init__(self):
+        self.reset()
 
+    def reset(self):
+        self.count = 0
+        self.mean = None
+        self.M2 = None
+       
+    
+    def update(self,value):
+        self.count+=1
+        if self.mean is None:
+            self.mean = value
+            self.M2 = self.mean-value
+        else:
+            delta = value - self.mean
+            self.mean += delta / self.count
+            delta2 = value - self.mean
+            self.M2 += delta * delta2
+
+    @property
+    def variance(self):
+        if self.count < 2:
+            return float('nan')
+        else:
+            return self.M2/self.count
+    
+    @property
+    def sampleVariance(self):
+        if self.count < 2:
+            return float('nan')
+        else:
+            return self.M2/(self.count-1)
