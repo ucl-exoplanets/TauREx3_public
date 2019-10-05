c088c9c8df7d04adb46a3783d41d4012c19d9b24
Ahmed Al-Refaie <alrefaie@desy.de>
2019-10-05 19:29:32 +0200

    Should work for any pickle now

--- tools/pickle_to_properformat.py
+++ tools/pickle_to_properformat.py
@@ -20,24 +20,29 @@ if __name__ == "__main__":
         splits = pathlib.Path(args.pickle).stem.split('.')
         mol_name = splits[0]
     print('Running for molecule ', mol_name)
-    with open(args.pickle,'rb') as pfd:
-        with h5py.File(args.output,'w') as fd:
-            xsec_pickle = pickle.load(pfd)
-            fd.create_dataset('bin_edges',data=xsec_pickle['wno'],shape=xsec_pickle['wno'].shape)
-            print('stored binedges', xsec_pickle['wno'])
-            fd.create_dataset('t',data=xsec_pickle['t'],shape=xsec_pickle['t'].shape)
-            print('stored temp', xsec_pickle['t'])
-            press = fd.create_dataset('p',data=xsec_pickle['p'],shape=xsec_pickle['p'].shape)
-            fd.create_dataset('xsecarr',data=xsec_pickle['xsecarr'],shape=xsec_pickle['xsecarr'].shape)
-            press.attrs['units'] = args.p_unit
-            print('stored pressure', xsec_pickle['p'])
-            print('Pressure units are', args.p_unit)
-            fd.create_dataset('mol_name',data=mol_name)
-            fd.create_dataset('key_iso_II',data='pickle')
+
+    with h5py.File(args.output,'w') as fd:
+        try:
+            with open(args.pickle,'rb') as pfd:
+                xsec_pickle = pickle.load(pfd)
+        except UnicodeDecodeError:
+            with open(args.pickle,'rb') as pfd:
+                xsec_pickle = pickle.load(pfd, encoding='latin1')
+        fd.create_dataset('bin_edges',data=xsec_pickle['wno'],shape=xsec_pickle['wno'].shape)
+        print('stored binedges', xsec_pickle['wno'])
+        fd.create_dataset('t',data=xsec_pickle['t'],shape=xsec_pickle['t'].shape)
+        print('stored temp', xsec_pickle['t'])
+        press = fd.create_dataset('p',data=xsec_pickle['p'],shape=xsec_pickle['p'].shape)
+        fd.create_dataset('xsecarr',data=xsec_pickle['xsecarr'],shape=xsec_pickle['xsecarr'].shape)
+        press.attrs['units'] = args.p_unit
+        print('stored pressure', xsec_pickle['p'])
+        print('Pressure units are', args.p_unit)
+        fd.create_dataset('mol_name',data=mol_name)
+        fd.create_dataset('key_iso_II',data='pickle')
 
         from taurex.opacity.hdf5opacity import HDF5Opacity
         hdf5 = HDF5Opacity(args.output)
         print('Pressure in pascal is ',hdf5.pressureGrid)
         print('Molecule name in HDF5 is',hdf5.moleculeName)
-        
+
         #Testing with
