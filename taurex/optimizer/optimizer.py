Actual variance fix that isnt shitty

taurex/optimizer/optimizer.py
@@ -490,7 +490,7 @@ class Optimizer(Logger):
         rank = mpi.get_rank()
         size = mpi.nprocs()
 
-
+        enableLogging()
         self.info('I will only iterate through partitioned %s points (the rest is in parallel)',len(sample_list)//size)
         disableLogging()
         count= 0
@@ -499,7 +499,9 @@ class Optimizer(Logger):
             self.update_model(parameters)
 
             if rank ==0 and count % 10 ==0 and count >0:
-                self.error('Progress {}%'.format(count*100.0        /(len(sample_list)/size)))
+                enableLogging()
+                self.info('Progress {}%'.format(count*100.0        /(len(sample_list)/size)))
+                disableLogging()
 
             count +=1
             weights.append(weight)
@@ -511,30 +513,26 @@ class Optimizer(Logger):
             inactive_gases.update(self._model.chemistry.inactiveGasMixProfile,weight=weight)
             binned_spectrum.update(binned,weight=weight)
             native_spectrum.update(native,weight=weight)
+
+        
+        if len(weights) == 0:
+            weight = 0.0
+            weights.append(0.0)
+            native_grid,native,tau,_ = self._model.model(wngrid=binning,cutoff_grid=False)
+            binned = self._binner.bindown(native_grid,native)[1]
+            #tau_profile.update(tau,weight=weight)
+            tp_profiles.update(self._model.temperatureProfile,weight=weight)
+            active_gases.update(self._model.chemistry.activeGasMixProfile,weight=weight)
+            inactive_gases.update(self._model.chemistry.inactiveGasMixProfile,weight=weight)
         enableLogging()
+        tp_std = np.sqrt(tp_profiles.parallelVariance())
+        active_std = np.sqrt(active_gases.parallelVariance())
+        inactive_std = np.sqrt(inactive_gases.parallelVariance())
 
+        #tau_std = np.sqrt(tau_profile.parallelVariance())
 
-        total_counts = sum(allgather(count))
-
-        if total_counts > 0:
-            tp_std = np.sqrt(tp_profiles.parallelVariance())
-            active_std = np.sqrt(active_gases.parallelVariance())
-            inactive_std = np.sqrt(inactive_gases.parallelVariance())
-
-            #tau_std = np.sqrt(tau_profile.parallelVariance())
-
-            binned_std = np.sqrt(binned_spectrum.parallelVariance())
-            native_std = np.sqrt(native_spectrum.parallelVariance())
-        else:
-            self.warning('WEIGHTS ARE ALL ZERO, SETTING PROFILES STD TO ZERO')
-            tp_std = np.zeros_like(tp_profiles)
-            active_std = np.zeros_like(active_gases)
-            inactive_std = np.zeros_like(inactive_gases)
-
-           # tau_std = np.zeros_like(tau_profile)
-
-            binned_std = np.zeros_like(binned_spectrum)
-            native_std = np.zeros_like(native_spectrum)
+        binned_std = np.sqrt(binned_spectrum.parallelVariance())
+        native_std = np.sqrt(native_spectrum.parallelVariance())
         tau_std = None
         return tp_std,active_std,inactive_std,tau_std,binned_std,native_std
 

taurex/util/math.py
@@ -114,40 +114,32 @@ class OnlineVariance(object):
 
 
     def combine_variance(self,averages, variances, counts):
-        
-        safe_idx = [i for i,a in enumerate(averages) if a is not None] 
-        
-
-        averages = [averages[idx] for idx in safe_idx]
-        variances = [variances[idx] for idx in safe_idx]
-        counts = [counts[idx] for idx in safe_idx]
-
-        average = np.average(averages, weights=counts, axis=0)
+        average = np.average(averages, weights=counts,axis=0)
 
         size = np.sum(counts)
         
         counts = np.array(counts) * size/np.sum(counts)
-        if hasattr(average, '__len__'):
-            average = average[None, ...]
+        if hasattr(average,'__len__'):
+            average = average[None,...]
 	    #for x in range(1,len(variances.shape
-            for x in range(1, len(average.shape)):
-                counts = counts[:, None]
+            for x in range(1,len(average.shape)):
+                counts = counts[:,None]
         
         squares = counts*variances
         squares += counts*(average - averages)**2
 
-        return average, np.sum(squares, axis=0)/size
+        return average,np.sum(squares,axis=0)/size
     def parallelVariance(self):
         from taurex import mpi
 
         variance = self.variance
         if variance is np.nan:
-            variance = None
+            variance = 0
         
 
         mean = self.mean
         if mean is None:
-            mean = None
+            mean = 0.0
 	
 
         variances = mpi.allgather(variance)