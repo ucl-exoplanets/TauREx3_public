32bbf80f4a93b0dc1d9ffa9d3a1b5ef87de60a68
Ahmed Al-Refaie <alrefaie@desy.de>
2019-10-11 01:31:43 +0200

    temperature aliased for easy access

---
+++ taurex/temperature.py
@@ -0,0 +1,5 @@
+from .data.profiles.temperature import Isothermal
+from .data.profiles.temperature import Guillot2010
+from .data.profiles.temperature import Rodgers2000
+from .data.profiles.temperature import TemperatureProfile
+from .data.profiles.temperature import TemperatureFile
