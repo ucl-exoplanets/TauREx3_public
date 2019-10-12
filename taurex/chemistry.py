10e8279d3e1836b5debf4f9a80a5ddceb78c5452
Ahmed Al-Refaie <alrefaie@desy.de>
2019-10-12 13:05:29 +0100

    Chemistry is aliased

---
+++ taurex/chemistry.py
@@ -0,0 +1,5 @@
+from .data.profiles.chemistry import ChemistryFile
+from .data.profiles.chemistry import Chemistry
+from .data.profiles.chemistry import TaurexChemistry
+from .data.profiles.chemistry import ConstantGas
+from .data.profiles.chemistry import TwoLayerGas
