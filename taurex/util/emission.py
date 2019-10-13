2842d135626d88c412e3af6562f646a60f7192dc
Ahmed Al-Refaie <alrefaie@desy.de>
2019-10-13 03:38:26 +0100

    faster blackbody calc

--- taurex/util/emission.py
+++ taurex/util/emission.py
@@ -5,39 +5,53 @@ import numpy as np
 import ctypes as C
 from scipy.stats.mstats_basic import tmean
 
-def black_body(lamb, temp):
-    """Computes black body radiation spectrum
 
-    Parameters
-    ----------
-    lambda: array
-        Wavenumber array in cm-1
-    
-    temp: float
-        Temeprature in Kelvin
-
-    Returns
-    -------
-    array:
-        Blackbody spectrum
-    
-    """
-
-
+def black_body(lamb,temp):
+    import numexpr as ne
     h = 6.62606957e-34
     c = 299792458
     k = 1.3806488e-23
     pi= 3.14159265359
     
-    wl = 10000/lamb
+    wl = ne.evaluate('10000*1e-6/lamb')
 
-    exponent = np.exp((h * c) / (wl*1e-6 * k * temp))
-    BB = (pi* (2.0*h*c**2)/(wl*1e-6)**5) * (1.0/(exponent -1))
+    #exponent = ne.evaluate('exp((h * c) / (wl*1e-6 * k * temp))')
+    BB = ne.evaluate('(pi* (2.0*h*c**2)/(wl)**5) * (1.0/(exp((h * c) / (wl * k * temp))-1))*1e-6')
+    return BB
 
-#            exponent = exp((h * c) / ((10000./wngrid[wn])*1e-6  * kb * temperature[0]));
-#            BB_wl = ((2.0*h*pow(c,2))/pow((10000./wngrid[wn])*1e-6,5) * (1.0/(exponent - 1)))* 1e-6; // (W/m^2/micron)
+# def black_body(lamb, temp):
+#     """Computes black body radiation spectrum
 
-#     exponent = np.exp((con.h * con.c) / (lamb *1e-6 * con.k * temp))
-#     BB = (np.pi* (2.0*con.h*con.c**2)/(lamb*1e-6)**5) * (1.0/(exponent -1))
+#     Parameters
+#     ----------
+#     lambda: array
+#         Wavenumber array in cm-1
     
-    return BB * 1e-6
+#     temp: float
+#         Temeprature in Kelvin
+
+#     Returns
+#     -------
+#     array:
+#         Blackbody spectrum
+    
+#     """
+
+
+#     h = 6.62606957e-34
+#     c = 299792458
+#     k = 1.3806488e-23
+#     pi= 3.14159265359
+    
+#     wl = 10000/lamb
+
+#     exponent = np.exp((h * c) / (wl*1e-6 * k * temp))
+#     BB = (pi* (2.0*h*c**2)/(wl*1e-6)**5) * (1.0/(exponent -1))
+
+# #            exponent = exp((h * c) / ((10000./wngrid[wn])*1e-6  * kb * temperature[0]));
+# #            BB_wl = ((2.0*h*pow(c,2))/pow((10000./wngrid[wn])*1e-6,5) * (1.0/(exponent - 1)))* 1e-6; // (W/m^2/micron)
+
+# #     exponent = np.exp((con.h * con.c) / (lamb *1e-6 * con.k * temp))
+# #     BB = (np.pi* (2.0*con.h*con.c**2)/(lamb*1e-6)**5) * (1.0/(exponent -1))
+    
+#     return BB * 1e-6
