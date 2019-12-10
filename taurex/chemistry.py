ffd4d8be78eaa9e1a17d608566b8a01e7f2ef815
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2019-12-10 17:32:44 +0000

    chemistry now attemps to import ACE

--- taurex/chemistry.py
+++ taurex/chemistry.py
@@ -1,5 +1,12 @@
+
+
 from .data.profiles.chemistry import ChemistryFile
 from .data.profiles.chemistry.chemistry import Chemistry
 from .data.profiles.chemistry import TaurexChemistry
+try:
+    from .data.profiles.chemistry.acechemistry import ACEChemistry
+except ImportError:
+    print('Ace library not found. ACEChemistry could not be loaded')
+from .data.profiles.chemistry.gas.gas import Gas
 from .data.profiles.chemistry import ConstantGas
 from .data.profiles.chemistry import TwoLayerGas
