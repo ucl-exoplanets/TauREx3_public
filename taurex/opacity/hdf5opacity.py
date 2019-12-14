06e0b53defd11fe4f8ba0fef45cc3a8ca8ae8ccb
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2019-12-14 20:46:19 +0000

    autopep8 on opacities

--- taurex/opacity/hdf5opacity.py
+++ taurex/opacity/hdf5opacity.py
@@ -2,23 +2,24 @@ from .interpolateopacity import InterpolatingOpacity
 import pickle
 import numpy as np
 import pathlib
+
+
 class HDF5Opacity(InterpolatingOpacity):
     """
     This is the base class for computing opactities
 
     """
-    
-    def __init__(self,filename,interpolation_mode='exp',in_memory=False):
+
+    def __init__(self, filename, interpolation_mode='exp', in_memory=False):
         super().__init__('HDF5Opacity:{}'.format(pathlib.Path(filename).stem[0:10]),
-                        interpolation_mode=interpolation_mode)
+                         interpolation_mode=interpolation_mode)
 
         self._filename = filename
         self._molecule_name = None
         self._spec_dict = None
         self.in_memory = in_memory
         self._load_hdf_file(filename)
-        
-        
+
     @property
     def moleculeName(self):
         return self._molecule_name
@@ -27,28 +28,26 @@ class HDF5Opacity(InterpolatingOpacity):
     def xsecGrid(self):
         return self._xsec_grid
 
-
-    def _load_hdf_file(self,filename):
+    def _load_hdf_file(self, filename):
         import h5py
         import astropy.units as u
-        #Load the pickle file
+        # Load the pickle file
         self.debug('Loading opacity from {}'.format(filename))
-        
-        self._spec_dict = h5py.File(filename,'r')
-        
+
+        self._spec_dict = h5py.File(filename, 'r')
+
         self._wavenumber_grid = self._spec_dict['bin_edges'][:]
 
         #temperature_units = self._spec_dict['t'].attrs['units']
         #t_conversion = u.Unit(temperature_units).to(u.K).value
 
-        self._temperature_grid = self._spec_dict['t'][:]#*t_conversion
-        
+        self._temperature_grid = self._spec_dict['t'][:]  # *t_conversion
 
         pressure_units = self._spec_dict['p'].attrs['units']
         try:
             p_conversion = u.Unit(pressure_units).to(u.Pa)
         except:
-            p_conversion = u.Unit(pressure_units,format="cds").to(u.Pa)
+            p_conversion = u.Unit(pressure_units, format="cds").to(u.Pa)
 
         self._pressure_grid = self._spec_dict['p'][:]*p_conversion
 
@@ -59,8 +58,8 @@ class HDF5Opacity(InterpolatingOpacity):
 
         self._resolution = np.average(np.diff(self._wavenumber_grid))
         self._molecule_name = self._spec_dict['mol_name'][()]
-        
-        if isinstance(self._molecule_name,np.ndarray):
+
+        if isinstance(self._molecule_name, np.ndarray):
             self._molecule_name = self._molecule_name[0]
 
         self._min_pressure = self._pressure_grid.min()
@@ -75,7 +74,7 @@ class HDF5Opacity(InterpolatingOpacity):
     @property
     def temperatureGrid(self):
         return self._temperature_grid
-    
+
     @property
     def pressureGrid(self):
         return self._pressure_grid
@@ -84,6 +83,4 @@ class HDF5Opacity(InterpolatingOpacity):
     def resolution(self):
         return self._resolution
 
-
-
-        #return factor*(q_11*(Pmax-P)*(Tmax-T) + q_21*(P-Pmin)*(Tmax-T) + q_12*(Pmax-P)*(T-Tmin) + q_22*(P-Pmin)*(T-Tmin))
+        # return factor*(q_11*(Pmax-P)*(Tmax-T) + q_21*(P-Pmin)*(Tmax-T) + q_12*(Pmax-P)*(T-Tmin) + q_22*(P-Pmin)*(T-Tmin))
--- taurex/opacity/opacity.py
+++ taurex/opacity/opacity.py
@@ -1,20 +1,20 @@
 from taurex.log import Logger
 import numpy as np
 
+
 class Opacity(Logger):
     """
     This is the base class for computing opactities
 
     """
-    
-    def __init__(self,name):
-        super().__init__(name)
 
+    def __init__(self, name):
+        super().__init__(name)
 
     @property
     def resolution(self):
         raise NotImplementedError
-    
+
     @property
     def moleculeName(self):
         raise NotImplementedError
@@ -26,30 +26,25 @@ class Opacity(Logger):
     @property
     def temperatureGrid(self):
         raise NotImplementedError
-    
+
     @property
     def pressureGrid(self):
         raise NotImplementedError
 
-
-    def compute_opacity(self,temperature,pressure,wngrid=None):
+    def compute_opacity(self, temperature, pressure, wngrid=None):
         raise NotImplementedError
 
-    
+    def opacity(self, temperature, pressure, wngrid=None):
 
-
-
-    def opacity(self,temperature,pressure,wngrid=None):
-        
         if wngrid is None:
             wngrid_filter = slice(None)
         else:
-            wngrid_filter = np.where( (self.wavenumberGrid >= wngrid.min()) & (self.wavenumberGrid <= wngrid.max() ))[0]
-    
+            wngrid_filter = np.where((self.wavenumberGrid >= wngrid.min()) & (
+                self.wavenumberGrid <= wngrid.max()))[0]
 
-        orig=self.compute_opacity(temperature,pressure,wngrid_filter)
+        orig = self.compute_opacity(temperature, pressure, wngrid_filter)
 
-        if wngrid is None or np.array_equal( self.wavenumberGrid.take(wngrid_filter),wngrid):
+        if wngrid is None or np.array_equal(self.wavenumberGrid.take(wngrid_filter), wngrid):
             return orig
         else:
             # min_max =  (self.wavenumberGrid <= wngrid.max() ) & (self.wavenumberGrid >= wngrid.min())
