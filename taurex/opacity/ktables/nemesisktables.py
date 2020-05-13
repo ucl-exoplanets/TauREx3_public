24fb141fb8f3a04c17611781de5371c6fa0a330d
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2020-05-13 17:33:48 +0100

    decoded nemesis format

--- taurex/opacity/ktables/nemesisktables.py
+++ taurex/opacity/ktables/nemesisktables.py
@@ -1,4 +1,4 @@
-from .interpolateopacity import InterpolatingOpacity
+from ..interpolateopacity import InterpolatingOpacity
 import pickle
 import numpy as np
 import pathlib
@@ -9,7 +9,7 @@ class NemesisKTables(InterpolatingOpacity):
     This is the base class for computing opactities
     """
     
-    def __init__(self,filename,interpolation_mode='linear'):
+    def __init__(self, filename, interpolation_mode='linear'):
         super().__init__('NemesisKtable:{}'.format(pathlib.Path(filename).stem[0:10]),
                         interpolation_mode=interpolation_mode)
 
@@ -17,8 +17,7 @@ class NemesisKTables(InterpolatingOpacity):
         self._molecule_name = None
         self._spec_dict = None
         self._resolution = None
-        self._load_pickle_file(filename)
-        
+        self._decode_ktables(filename)
         
     @property
     def moleculeName(self):
@@ -28,8 +27,60 @@ class NemesisKTables(InterpolatingOpacity):
     def xsecGrid(self):
         return self._xsec_grid
 
-    def decode_ktables(self,filename):
-        pass
+    def _decode_ktables(self, filename):
+        self.debug('Reading NEMESIS FORMAT')
+        nem_file_float = np.fromfile(filename, dtype=np.float32)
+        nem_file_int = nem_file_float.view(np.int32)
+        array_counter = 0
+        self.debug('MAGIC NUMBER: %s', nem_file_int[0])
+        wncount = nem_file_int[1]
+        self.debug('WNCOUNT = %s', wncount)
+        wnstart = nem_file_float[2]
+        self.debug('WNSTART = %s um',wnstart)
+        float_num = nem_file_float[3]
+        self.debug('FLOAT: %s INT: %s',float_num, nem_file_int[4])
+
+        num_pressure = nem_file_int[5]
+        num_temperature = nem_file_int[6]
+        num_quads = nem_file_int[7]
+
+        self.debug('NP: %s NT: %s NQ: %s', num_pressure, num_temperature,
+                   num_quads)
+        self.debug('UNKNOWN VALUES: %s %s', nem_file_int[8], nem_file_int[9])
+
+        array_counter += 10+num_quads*2
+        self._samples, self._weights = \
+            nem_file_float[10:array_counter].reshape(2, -1)
+        self.debug('Samples: %s, Weights: %s', self._samples, self._weights)
+        self.debug('%s', nem_file_int[array_counter])
+        array_counter += 1
+        self.debug('%s', nem_file_int[array_counter])
+        array_counter += 1
+        self._pressure_grid = nem_file_float[array_counter:array_counter+num_pressure]
+        self.debug('Pgrid: %s',self._pressure_grid)
+        array_counter+=num_pressure
+        self._temperature_grid = nem_file_float[array_counter:array_counter+num_temperature]
+        array_counter += num_temperature
+        self.debug('Tgrid: %s',self._temperature_grid)
+        self._wavenumber_grid = 10000/nem_file_float[array_counter:array_counter+wncount]
+        self._wavenumber_grid = self._wavenumber_grid[::-1]
+        array_counter += wncount
+        self.debug('Wngrid: %s',self._wavenumber_grid)
+
+        self._xsec_grid=nem_file_float[array_counter:].reshape(wncount,num_quads,num_pressure, num_temperature) * 1e-20
+        self._xsec_grid = self._xsec_grid.transpose((2,3,0,1))
+        self._xsec_grid = self._xsec_grid[:,:,::-1,:]
+        self._min_pressure = self._pressure_grid.min()
+        self._max_pressure = self._pressure_grid.max()
+        self._min_temperature = self._temperature_grid.min()
+        self._max_temperature = self._temperature_grid.max()
+        # 0
+        # 0
+        # PRESSURE POINTS
+        # TEMPERATUREPOINTS
+        # WNGRID
+        # KCOEFFS SCALED 1e-20    SHAPE(WNGRID,????)
+
 
     # def _load_pickle_file(self, filename):
 
