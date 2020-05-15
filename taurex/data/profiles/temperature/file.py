a78aaf5c727f75378cec0068fa8e4bb99b5db759
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2020-05-15 19:07:29 +0100

    Fixed bug in conversion factor for pressure

--- taurex/data/profiles/temperature/file.py
+++ taurex/data/profiles/temperature/file.py
@@ -22,7 +22,7 @@ class TemperatureFile(TemperatureArray):
         temperature_arr = None
 
         convertT = conversion_factor(temp_units, 'K')
-        convertP = conversion_factor(press_units)
+        convertP = conversion_factor(press_units, 'Pa')
 
         if press_col is not None:
             arr = np.loadtxt(filename, skiprows=skiprows, 
