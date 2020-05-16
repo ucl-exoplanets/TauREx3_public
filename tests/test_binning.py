b2add30c3d7c1d40754924bedfa6cd2af26c4dea
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2020-05-16 19:19:16 +0100

    Converting to pytest

---
+++ tests/test_binning.py
@@ -0,0 +1,23 @@
+import pytest
+import numpy as np
+from taurex.binning import FluxBinner, SimpleBinner
+
+
+def test_fluxbinner(spectra):
+    wngrid = np.linspace(400, 20000, 100)
+    fb = FluxBinner(wngrid=wngrid)
+    wn, sp, _, _ = fb.bindown(*spectra)
+
+    assert(wngrid.shape[0] == wn.shape[0])
+
+    assert np.mean(sp) == pytest.approx(np.mean(spectra[1]), rel=0.1)
+
+
+def test_simplebinner(spectra):
+    wngrid = np.linspace(400, 20000, 100)
+    fb = SimpleBinner(wngrid=wngrid)
+    wn, sp, _, _ = fb.bindown(*spectra)
+
+    assert(wngrid.shape[0] == wn.shape[0])
+
+    assert np.mean(sp) == pytest.approx(np.mean(spectra[1]), rel=0.1)
