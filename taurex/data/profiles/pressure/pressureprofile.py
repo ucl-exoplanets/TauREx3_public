Tested pressure profile, fixed bug where variance was not nan in parallel case < 2

taurex/data/profiles/pressure/pressureprofile.py
@@ -2,7 +2,7 @@ from taurex.log import Logger
 from taurex.data.fittable import fitparam, Fittable
 import numpy as np
 from taurex.output.writeable import Writeable
-
+import math
 
 class PressureProfile(Fittable, Logger, Writeable):
     """
@@ -128,11 +128,14 @@ class SimplePressureProfile(PressureProfile):
         """
 
         # set pressure profile of layer boundaries
-        press_exp = np.linspace(np.log(self._atm_min_pressure),
-                                np.log(self._atm_max_pressure),
-                                self.nLevels)
-        self.pressure_profile_levels = np.exp(press_exp)[::-1]
-
+        # press_exp = np.linspace(np.log(self._atm_min_pressure),
+        #                       np.log(self._atm_max_pressure),
+        #                       self.nLevels)
+        # self.pressure_profile_levels = np.exp(press_exp)[::-1]
+        self.pressure_profile_levels = \
+            np.logspace(math.log10(self._atm_min_pressure),
+                        math.log10(self._atm_max_pressure),
+                        self.nLevels)[::-1]
         # get mid point pressure between levels (i.e. get layer pressure)
         # computing geometric
         # average between pressure at n and n+1 level

taurex/util/math.py
@@ -272,9 +272,11 @@ class OnlineVariance(object):
         #print('VARIANCES',variances)
         #print('AVERAGES',averages)
         #print('COUNTS',counts)
-        
-        finalvariance = self.combine_variance(averages,variances,counts)
-        return finalvariance[-1]
+        if sum(counts) < 2:
+            return np.nan
+        else:
+            finalvariance = self.combine_variance(averages,variances,counts)
+            return finalvariance[-1]
 
         
         

tests/test_pressure.py
@@ -1,12 +1,25 @@
 import pytest
-from taurex.pressure import SimplePressureProfile
-from hypothesis import given, assume
+from taurex.pressure import SimplePressureProfile, PressureProfile
+from hypothesis import given, assume, example
 from hypothesis.strategies import floats, integers
 import numpy as np
 
 
+@given(nlayers=integers())
+def test_base_pressure(nlayers):
+    if nlayers <= 0:
+        with pytest.raises(ValueError):
+            p = PressureProfile('test', nlayers)
+    else:
+        p = PressureProfile('test', nlayers)
+        
+        assert p.nLayers == nlayers
+        assert p.nLevels == nlayers + 1
+
+
 @given(min_pressure=floats(1e4, 1e6), max_pressure=floats(1e-5,1e5), 
        nlayers=integers(-100, 400))
+@example(min_pressure=10000.0, max_pressure=10000.000000000011, nlayers=1)
 def test_simple_pressure(min_pressure, max_pressure, nlayers):
 
     if min_pressure > max_pressure or nlayers <= 0:
@@ -26,12 +39,12 @@ def test_simple_pressure(min_pressure, max_pressure, nlayers):
         pressure_profile = sp.profile
 
         pressure_profile_levels = sp.pressure_profile_levels
-
-
         assert pressure_profile_levels.argmax() == 0 # Ensure maximum is the first
-        assert pressure_profile_levels.argmin() == nlayers-1 # ensure minimum is last
-        assert pressure_profile_levels.max() == max_pressure
-        assert pressure_profile_levels.min() == min_pressure
+        assert pressure_profile_levels.argmin() == nlayers # ensure minimum is last
+
+        # When the maximum floating point then max does not give exactly the same as max pressure
+        assert pressure_profile_levels.max() == pytest.approx(max_pressure, rel=1e-10)
+        assert pressure_profile_levels.min() == pytest.approx(min_pressure, rel=1e-10)
 
         # Test to ensure they are bounded correctly
         assert np.all(pressure_profile_levels[1:-1] < max_pressure)

tests/test_util/test_math.py
@@ -50,9 +50,11 @@ def test_online_variance(s):
     for x in s:
         onv.update(x)
     
-    var = onv.parallelVariance()
-    if s.shape[0] < 2:
-        assert np.all(np.isnan(var))
+    p_var = onv.parallelVariance()
+    var = onv.variance
+    if num_values < 2:
+        assert np.isnan(var)
+        assert np.isnan(p_var)
     else:
         assert np.sqrt(var) == pytest.approx(expected, rel=1e-6)
     #onv = Onli