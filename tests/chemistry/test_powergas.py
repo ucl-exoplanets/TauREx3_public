9788f2ddfb235b3f57bb105e35cfb5209824114a
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2020-09-30 18:23:48 +0100

    Powergas test to check auto does not overide user values

--- tests/chemistry/test_powergas.py
+++ tests/chemistry/test_powergas.py
@@ -48,5 +48,21 @@ def test_auto_profile(molecule, TP):
 
     assert np.all(ps_truth.mixProfile == ps_auto.mixProfile)
 
+    
+@pytest.mark.parametrize(
+    "molecule",
+    ['H2', 'H2O', 'TiO', 'VO', 'H-', 'Na', 'K'])
+@given(molecule_chosen=molecule_vmr(), alpha=floats(allow_nan=False),
+                    beta=floats(allow_nan=False), gamma=floats(allow_nan=False), TP=TPs())
+def test_auto_profile_override(molecule, molecule_chosen, alpha, beta, gamma, TP):
+    T, P, n = TP
+    mol, vmr = molecule_chosen
+    mol_name = mol[0]
+    ps_auto = PowerGas(molecule_name=molecule, profile_type='auto')
+    ps_truth = PowerGas(molecule_name=molecule, mix_ratio_surface=vmr, alpha=alpha, beta=beta,
+                  gamma=gamma, profile_type='auto')
 
+    ps_auto.initialize_profile(n, T, P, None)
+    ps_truth.initialize_profile(n, T, P, None)
 
+    assert np.all(ps_truth.mixProfile != ps_auto.mixProfile)
