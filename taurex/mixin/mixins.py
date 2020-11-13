6a053eab597807f7f9d889b0921ffa19e7be77aa
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2020-11-13 14:26:22 +0000

    An example mixin

--- taurex/mixin/mixins.py
+++ taurex/mixin/mixins.py
@@ -1,4 +1,26 @@
-from . import ChemistryMixin
-
+from . import ChemistryMixin, TemperatureMixin
+from taurex.core import fitparam
 class MakeFreeMixin(ChemistryMixin):
     pass
+
+class TempScaler(TemperatureMixin):
+
+    def __init__(self, scale_factor=1.0):
+        super().__init__()
+        self._scale_factor = scale_factor
+    
+    @fitparam(param_name='T_scale')
+    def scaleFactor(self):
+        return self._scale_factor
+
+    @scaleFactor.setter
+    def scaleFactor(self, value):
+        self._scale_factor = value
+
+    @property
+    def profile(self):
+        return super().profile*self._scale_factor
+
+    @classmethod
+    def input_keywords(self):
+        return ['tempscalar', ]
